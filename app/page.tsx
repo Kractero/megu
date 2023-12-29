eb37285246791cb44a9268d0d69bc6c158a35e48
Kractero <kractero@sideroca.com>
2023-12-29 10:03:34 -0700

    dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { CardForm } from '@/components/CardForm'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center pt-24 md:pt-48">
-      <h2 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+      <h2 className="font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
         Megumin
       </h2>
       <p className="text-xs text-center mt-4">(Market Evaluation Graphs, Utilizing Market Information (necessarily))</p>
